4bfe1bc629b129e396c3793c5cd315dd7a8a76b2
akbars95 <Dmitri.Minzat@endava.com>
2016-11-23 18:38:33 +0200

    maven-failsafe-plugin

--- activeMQ/src/main/java/com/mtsmda/jms/activeMQ/ActiveMQConnection.java
+++ activeMQ/src/main/java/com/mtsmda/jms/activeMQ/ActiveMQConnection.java
@@ -7,8 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
  */
 public class ActiveMQConnection {
 
-    @Autowired
-    @
+
 
     public void connection(){
 
---
+++ activeMQ/src/test/java/com/mtsmda/jms/activeMQ/SimpleTest.java
@@ -0,0 +1,17 @@
+package com.mtsmda.jms.activeMQ;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by dminzat on 11/23/2016.
+ */
+public class SimpleTest {
+
+    @Test
+    public void test1001(){
+        assertTrue("string".startsWith("str"));
+    }
+
+}
